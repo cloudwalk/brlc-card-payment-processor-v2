a5b6b01558ef2f21a058e93ea64be9585591d85a
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-09-19 12:38:17 +0100

    pragma solidity

--- contracts/hookable/CardPaymentProcessorHookable.sol
+++ contracts/hookable/CardPaymentProcessorHookable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.24;
 
 import { ICardPaymentProcessorHookable } from "./interfaces/ICardPaymentProcesorHookable.sol";
 import { CardPaymentProcessorHookableStorageLayout } from "./CardPaymentProcessorHookableStorageLayout.sol";
--- contracts/interfaces/ICashbackController.sol
+++ contracts/interfaces/ICashbackController.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.24;
 
 import { IAfterPaymentMadeHook } from "../hookable/interfaces/ICardPaymentProcessorHooks.sol";
 import { IAfterPaymentUpdatedHook } from "../hookable/interfaces/ICardPaymentProcessorHooks.sol";
